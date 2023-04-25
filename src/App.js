696f6395f0829e3a15488440096f315fa28a59f6
yildirimkadir <yildirimkad34@gmail.com>
2023-04-25 17:33:11 +0200

    "updated"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import { userValidation } from './validation';
 
 function App() {
+  const createUser = async (event) => {
+    event.preventDefault();
+    let formData = {
+      name: event.target[0].value,
+      email: event.target[1].value,
+      password: event.target[2].value,
+    };
+    console.log(formData);
+    const isValid = await userValidation.isValid(formData);
+    console.log(isValid);
+    event.target.reset()
+  };
+
+  //async await dememzin sebebi call back den gelen veriye isValid uygulanmasi.    
+
+
+
   return (
-    <div className="App"> Hallo
+    <div className="App">
+      <form className="form" onSubmit={createUser}>
+        <input type="text" placeholder='schreib your name' />
+        <input type="text" placeholder='schreib your email' />
+        <input type="password" placeholder='schreib your password' />
+        <input type="submit" className='submit' />
+      </form>
     </div>
   );
 }
